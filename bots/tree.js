584423912155fdcf62ea070ce49e751c7a344cff
BattleDog249 <lgray@protonmail.com>
2022-12-21 12:04:55 -0800

    Add test variables for Mehri tree farm

--- bots/tree.js
+++ bots/tree.js
@@ -43,17 +43,29 @@ width = 4;
 // Testing coords
 //startX = 3247.5;
 //startZ = -2396.5;
+// First level of Mehri tree farm
+//startX = 3247.5;
+//startZ = -2397.5;
 // Second level of Mehri tree farm
+//startX = 3247.5;
+//startZ = -2396.5;
+// Third level of Mehri tree farm
 startX = 3247.5;
-startZ = -2396.5;
+startZ = -2395.5;
 
 // Assign coords of last tree opposite of starting coords
 // Testing coords
 //endX = -132.5;
 //endZ = 6.5;
+// First level of Mehri tree farm
+//endX = 3172.5;
+//endZ = -2332.5;
 // Second level of Mehri tree farm
+//endX = 3172.5;
+//endZ = -2331.5;
+// Third level of Mehri tree farm
 endX = 3172.5;
-endZ = -2331.5;
+endZ = -2330.5;
 
 // Function to equip a given item on the hotbar if located in inventory
 // name = "minecraft:itemname"
