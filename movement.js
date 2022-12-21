e1fa12b5d79424419d4d9bb2f8f0dfa6a39459df
BattleDog249 <lgray@protonmail.com>
2022-12-20 19:29:40 -0800

    Add pick() function

--- movement.js
+++ movement.js
@@ -1,3 +1,36 @@
+// Function for equipping a given item
+function pick(name, hotbar = null, dmg = -1) {
+    inv = Player.openInventory();
+    slots = inv.getMap();
+    
+    if (hotbar == null) {
+        hotbar = inv.getSelectedHotbarSlotIndex();
+    }
+    
+    slot = slots["hotbar"][inv.getSelectedHotbarSlotIndex()];
+    item = inv.getSlot(slot);
+    dura = item.getMaxDamage() - item.getDamage();
+
+    if (item.getItemId() === name && (dmg == -1 || dura > dmg)) {
+        inv.close();
+        return true;
+    }
+    
+    for (slot of Array.from(slots.get("main")).concat(slots.get("hotbar"))) {
+        let item = inv.getSlot(slot);
+        if (item.getItemId() === name && (dmg == -1 || dura > dmg)) {
+            //Chat.log(`Found ${item.getItemId()} at slot ${slot}.`);
+            inv.swap(slot, slots["hotbar"][hotbar]);
+            Time.sleep(250);
+            inv.setSelectedHotbarSlotIndex(parseInt(slot));
+            inv.close();
+            return true;
+        }
+    }
+    inv.close();
+    return false;
+}
+
 function walkTo(x = null, z = null, precise = false, timeout = null) {
     pos = Player.getPlayer().getPos();
     if (x == null) {
@@ -50,7 +83,7 @@ function walkTo(x = null, z = null, precise = false, timeout = null) {
     return true;
 }
 
-Chat.log("Starting!")
+Chat.log("Starting!");
 
 width = 4;
 
